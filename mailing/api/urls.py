d24b9bc97b5f3b0bff6b16b130a34f5c79f6154b
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-03-27 23:48:56 +0200

    fixed url typos

--- mailing/api/urls.py
+++ mailing/api/urls.py
@@ -4,6 +4,6 @@ from mailing.api import views
 app_name = 'mailing-api'
 
 urlpatterns = [
-     path('create-mail-icampaign/', views.create_campaign, name='api-create-campaign'),
+     path('create-mail-campaign/', views.create_campaign, name='api-create-campaign'),
      path('update-mail-campaign/<uuid:campaign_uuid>/', views.update_campaign, name='api-update-campaign'),
 ]
